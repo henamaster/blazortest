7db4e4eb992b5c4752237666ea41a17a9cca85c3
Hena Master Östlund <hma@hv.se>
2020-11-04 11:11:32 +0100

    starwars!

---
+++ BlazorApp2/Models/StarWarsCharactersResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Models
+{
+    public class StarWarsCharactersResult
+    {
+        [JsonPropertyName("results")]
+        public StarWarsCharacter[] StarWarsCharacters { get; set; }
+    }
+}
--- BlazorApp2/Program.cs
+++ BlazorApp2/Program.cs
@@ -17,7 +17,7 @@ namespace BlazorApp2
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://swapi.dev/api/") });
 
             await builder.Build().RunAsync();
         }
